3dade7da9e57392e4fd9cb76d0ac8d730aa26f24
NotYou404 <dominik.reinartz@hotmail.com>
2023-11-11 21:47:29 +0100

    added featured to home

--- paulhub/app.py
+++ paulhub/app.py
@@ -2,13 +2,47 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     context = {
         "title": "Home",
-        "header_text": "Happy October!"
+        "header_text": "Happy October!",
+        "trending": (
+            (
+                {
+                    "name": "menu.png",
+                    "alt": "Petition against the November",
+                },
+                {
+                    "func": "about",
+                    "text": "Petition against the November",
+                },
+            ),
+            (
+                {
+                    "name": "menu.png",
+                    "alt": "Personal stuff",
+                },
+                {
+                    "func": "about",
+                    "text": "Personal stuff",
+                }
+            ),
+            (
+                {
+                    "name": "menu.png",
+                    "alt": "December preparations",
+                },
+                {
+                    "func": "about",
+                    "text": "December preparations",
+                }
+            ),
+        ),
     }
-    return render_template("base.html", **context)
+    return render_template("home.html", **context)
+
 
 @app.route("/about")
 def about():
