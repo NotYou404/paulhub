0210752a8fdb1dc4dd21b11856c7ad7a63d3cc79
NotYou404 <dominik.reinartz@hotmail.com>
2023-11-11 22:23:43 +0100

    somewhat finished home, added footer

--- paulhub/app.py
+++ paulhub/app.py
@@ -1,45 +1,36 @@
 from flask import Flask, render_template
+from pathlib import Path
+import json
+from datetime import datetime
 
 app = Flask(__name__)
 
 
+MonthDataType = dict[str, list[list[dict[str, str]]]]
+
+
+def get_month_data(month: int) -> MonthDataType:
+    """
+    Retrieve trending, latest and recommendations information about a
+    specific month.
+
+    :param month: The month as int from 1 to 12 (Jan-Dec)
+    :type month: int
+    :return: The datatype to be passed to the template as `month_data`
+    :rtype: MonthDataType
+    """
+    data_path = Path(__file__).parent / "data" / "home.json"
+    with open(data_path, "r", encoding="utf-8") as fp:
+        return json.load(fp)[str(month)]
+
+
 @app.route("/")
 def home():
+    month = datetime.today().month
     context = {
         "title": "Home",
         "header_text": "Happy October!",
-        "trending": (
-            (
-                {
-                    "name": "menu.png",
-                    "alt": "Petition against the November",
-                },
-                {
-                    "func": "about",
-                    "text": "Petition against the November",
-                },
-            ),
-            (
-                {
-                    "name": "menu.png",
-                    "alt": "Personal stuff",
-                },
-                {
-                    "func": "about",
-                    "text": "Personal stuff",
-                }
-            ),
-            (
-                {
-                    "name": "menu.png",
-                    "alt": "December preparations",
-                },
-                {
-                    "func": "about",
-                    "text": "December preparations",
-                }
-            ),
-        ),
+        "month_data": get_month_data(month),
     }
     return render_template("home.html", **context)
 
@@ -48,5 +39,6 @@ def home():
 def about():
     context = {
         "title": "About",
+        "header_text": "About"
     }
     return render_template("base.html", **context)
